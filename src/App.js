a81bb5fd8c3bd6e91f589533e4943a5db1ce27f6
tuananh2403 <tuananh240399@gmail.com>
2022-05-17 11:09:58 +0700

    show infor staff

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap'
+import StaffList from './components/StaffList'
+import { STAFFS } from './shared/staffs';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes : STAFFS
+    };
+  }
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
             <NavbarBrand href="/">Danh Sách Nhân Viên</NavbarBrand>
         </Navbar>
+        <StaffList dishes={this.state.dishes}/>
     </div>
   );
 }
--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -1,13 +1,27 @@
 import React,{ Component} from "react";
+import { Media } from "reactstrap";
 
  class StaffList extends Component {
      constructor(props) {
          super(props);
+         this.state ={}
      }
      render() {
+         const menu = this.props.dishes.map((dish) => {
+             return (
+                 <div key ={dish.id} className="col-5 m-2 border">
+                     <Media tag ="li" style ={{listStyleType: 'none'}}>
+                         <Media body className="ml-3">
+                                <Media heading>{dish.name}</Media>
+                         </Media>
+                     </Media>
+                 </div>
+             )
+         })
          return (
             <div className="container">
                 <div className="row">
+                    {menu}
                 </div>
             </div>
          );
