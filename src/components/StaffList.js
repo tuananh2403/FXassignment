64456c48792323a6f3e0cdb9165c0601efcb10e4
tuananh2403 <tuananh240399@gmail.com>
2022-05-17 10:43:25 +0700

    create component StaffList

---
+++ src/components/StaffList.js
@@ -0,0 +1,16 @@
+import React,{ Component} from "react";
+
+ class StaffList extends Component {
+     constructor(props) {
+         super(props);
+     }
+     render() {
+         return (
+            <div className="container">
+                <div className="row">
+                </div>
+            </div>
+         );
+     }
+ }
+ export default StaffList;
