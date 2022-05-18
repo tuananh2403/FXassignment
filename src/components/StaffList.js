9fcee55d1f57adb64e467ad5dfdfe25df0e8b2a4
tuananh2403 <tuananh240399@gmail.com>
2022-05-18 11:46:24 +0700

    show information staff

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -1,20 +1,50 @@
 import React,{ Component} from "react";
 import { Media } from "reactstrap";
+import {Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle} from 'reactstrap'
 
  class StaffList extends Component {
      constructor(props) {
          super(props);
-         this.state ={}
+         this.state ={
+             selectedDish:null
+         }
      }
+     onDishSelected(dish) {
+         this.setState({selectedDish:dish})
+     }
+    renderDish(dish) {
+        if(dish !=null){
+            return(
+
+                <div className="col-12">
+                <Card> 
+                     <CardBody>
+                        <CardTitle className="name">Họ và Tên : {dish.name}</CardTitle>
+                        <CardText>Ngày Sinh:{dish.doB}</CardText>
+                        <CardText>Ngày vào công ty:{dish.startDate}</CardText>
+                        <CardText>Phòng Ban : {dish.department.name}</CardText>
+                        <CardText>Số ngày nghỉ còn lại : {dish.annualLeave}</CardText>
+                        <CardText>Số ngày làm thêm : {dish.overTime}</CardText>
+
+                     </CardBody>
+                </Card>
+            </div>
+                )
+        }else{
+            return (
+                <div></div>
+            )
+        }
+    }
      render() {
          const menu = this.props.dishes.map((dish) => {
              return (
-                 <div key ={dish.id} className="col-5 m-2 border">
-                     <Media tag ="li" style ={{listStyleType: 'none'}}>
-                         <Media body className="ml-3">
-                                <Media heading>{dish.name}</Media>
-                         </Media>
-                     </Media>
+                 <div key ={dish.id} className="col-12 col-sm-6 col-lg-4 mt-2">
+                     <Card onClick={() => this.onDishSelected(dish)}> 
+                            <CardBody body className="ml-5">
+                                 <CardTitle>{dish.name}</CardTitle>
+                            </CardBody>
+                     </Card>
                  </div>
              )
          })
@@ -22,6 +52,9 @@ import { Media } from "reactstrap";
             <div className="container">
                 <div className="row">
                     {menu}
+                </div>
+                <div className="row mt-2">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
          );
