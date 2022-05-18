98b2e227822c498cadf42a321625224857fed9e1
tuananh2403 <tuananh240399@gmail.com>
2022-05-18 12:46:52 +0700

    add button change colum

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -2,12 +2,14 @@ import React,{ Component} from "react";
 import { Media } from "reactstrap";
 import {Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle} from 'reactstrap';
 import dateFormat from "dateformat";
+import {Button} from "reactstrap"
 
  class StaffList extends Component {
      constructor(props) {
          super(props);
          this.state ={
-             selectedDish:null
+             selectedDish:null,
+             columnSelect:"col-12 col-sm-6 col-lg-4 mt-2"
          }
      }
      onDishSelected(dish) {
@@ -40,7 +42,7 @@ import dateFormat from "dateformat";
      render() {
          const menu = this.props.dishes.map((dish) => {
              return (
-                 <div key ={dish.id} className="col-12 col-sm-6 col-lg-4 mt-2">
+                 <div key ={dish.id} className='col-12 col-sm-6 col-lg-4 mt-2'>
                      <Card onClick={() => this.onDishSelected(dish)}> 
                             <CardBody body className="ml-5">
                                  <CardTitle>{dish.name}</CardTitle>
@@ -51,6 +53,16 @@ import dateFormat from "dateformat";
          })
          return (
             <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <Button color="danger" className="m-2" onClick={() => this.onCol}>1 Cột </Button>
+                        <Button color="danger" className="m-2">2 Cột </Button>
+                        <Button color="danger" className="m-2">3 Cột </Button>
+                        <Button color="danger" className="m-2">4 Cột </Button>
+                        <Button color="danger" className="m-2">5 Cột </Button>
+                        <Button color="danger" className="m-2">6 Cột </Button>
+                    </div>
+                </div>
                 <div className="row">
                     {menu}
                 </div>
